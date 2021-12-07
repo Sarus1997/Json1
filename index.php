36060ca1c9394088af42dbd92b2ace355f15b89f
Sarus1997 <77056021+Sarus1997@users.noreply.github.com>
2021-12-07 13:34:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
- 
     <table>
         <thead>
             <tr>
-                <th>ID</th><th>First Name</th><th>Last Name</th>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
             </tr>
         </thead>
-        <tbody id="tblStudent">
+        <tbody id="tblPost">
+            
         </tbody>
     </table>
 </body>
+
+
 <script>
- 
-function loadJSON(){
-    var url = "https://cj-android-demon.herokuapp.com/json2.php";
+    function LoadPosts() {
+        var url="https://jsonplaceholder.typicode.com/posts"
 
-    $.getJSON(url)
-        .done((data)=>{
-            console.log(data);
-            $each(data, (k, item)=>{
-            console.log(k);
-            console.log(item);
-            var line = "<tr>";
-                line = "<td>" + (k+1) + "</td>";
-                line = "<td>" + item.fname + "</td>";
-                line = "<td>"+ item.lname + "</td>";
-                line = "</tr>";
-                $("#tblStudent").append(line);
-        });
+        $.getJSON(url)
+            .done((data) =>{
+                $.each(data,(k,item) => {
+                    console.log(item);
+                    var line = "<tr>";
+                        line += "<td>" + item.id + "</td>"
+                        line += "<td>" + item.title + "</td>"
+                        line += "<td>" + item.userId + "</td>"
+                        line += "</tr>";
+                    $("#tblPost").append(line);
+                })      
             })
+            .fail((xhr,err,status) =>{
 
-        .fail((xhr, status, err)=>{
+            })
+    }
 
-        })
-            }
-
-        $(()=>{
-            loadJSON();
-        });
+    $(() => {
+        LoadPosts();
+    })
 </script>
-
 </html>
