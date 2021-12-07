8e3f5f02bb01df5bfa2e14f5c571dad02a0081c2
Sarus1997 <77056021+Sarus1997@users.noreply.github.com>
2021-12-07 10:46:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,51 +8,21 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <input type="number" name="" id="x" />
-    <input type="number" name="" id="y" />
-    <span id="txtResult"> - </span> 
-    <button  id="get1"> GET </button>
-    <button  id="get2"> GET </button>
-    <hr>
-    <button  id="json1"> JSON1 </button>
+    
 </body>
+    <button id="btnJSON1"> JSON 1</button>
 <script>
+    function loadJSON(){
+        var url = "https://cj-android-demon.herokuapp.com/json1.php"
+        $.get(url, (data, status)=>{
+            console.log(data);
+        });
+
+    }
     $(()=>{
-        $("#get1").click(()=>{
-            var x = $("#x").val();
-            var y = $("#y").val();
-            var url = "http://cj-android-demon.herokuapp.com/add.php?x=%22+x+%22&y=" + y;
-            console.log(url);
-            $.get(url, (data, status)=>{
-                console.log(data);
-                console.log(status);
-                $("#txtResult").text(data);
-            })
-        });
-        $("#get2").click(()=>{
-            var x = $("#x").val();
-            var y = $("#y").val();
-            var url = "http://cj-android-demon.herokuapp.com/add2.php";
-            console.log(url);
-            $.post(url, {
-                "x": x,
-                "y": y
-            }, (data, status)=>{
-                console.log(data);
-                console.log(status);
-                $("#txtResult").text(data);
-            });
+            alert("TOi")
 
-        });
-        $("#json1").click(()=>{
 
-            var url = "http://cj-android-demon.herokuapp.com/json2.php";
-            console.log(url);
-            $.get(url, (data, status)=>{
-                console.log(data);
-
-            })
-        });
     });
 </script>
 </html>
